cfb8af4bfaa001f035742c26c05b4856f9a0ddda
Alexey Alexeew <lelikboss97@gmail.com>
2021-08-27 18:15:39 +0300

    change

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './sass/main.scss';
+import debounce from 'lodash.debounce';
+import axios from 'axios';
+const refs = {
+  inputEl: document.querySelector('#input'),
+};
+
+refs.inputEl.addEventListener('input', debounce(onInputChange, 500));
+function onInputChange(e) {
+  const value = e.target.value;
+  console.log(value);
+  axios
+    .get(`https://restcountries.eu/rest/v2/name/${value}`)
+    .then(result => console.log(result.data))
+    .catch(err => console.log(err));
+}
